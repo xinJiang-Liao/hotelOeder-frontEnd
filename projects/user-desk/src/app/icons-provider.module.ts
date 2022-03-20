93bd90f8ae072df8298d20bdb95b5af6d038aa30
廖江鑫 <1069672898@qq.com>
2022-03-20 11:01:17 +0800

    dev(desk): 使用ng-zorro构建后台应用，并生成侧边栏，然后尝试添加nzIcon的图标(Build the backend application with NG-Zorro and generate the sidebar, then try adding the nzIcon icon)

---
+++ projects/user-desk/src/app/icons-provider.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { NZ_ICONS, NzIconModule } from 'ng-zorro-antd/icon';
+
+import {
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  FormOutline,
+  DashboardOutline,
+  LogoutOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons = [
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  DashboardOutline,
+  FormOutline,
+  LogoutOutline,
+];
+
+@NgModule({
+  imports: [NzIconModule],
+  exports: [NzIconModule],
+  providers: [{ provide: NZ_ICONS, useValue: icons }],
+})
+export class IconsProviderModule {}
