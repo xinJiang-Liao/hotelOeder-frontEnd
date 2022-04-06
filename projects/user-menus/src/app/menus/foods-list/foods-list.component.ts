f0c741d0df5e5c9a48de37fd03b02a7d9264ce22
廖江鑫 <1069672898@qq.com>
2022-04-07 00:43:43 +0800

    dev(user-menus):调整页面响应式布局

--- projects/user-menus/src/app/menus/foods-list/foods-list.component.ts
+++ projects/user-menus/src/app/menus/foods-list/foods-list.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-foods-list',
   templateUrl: './foods-list.component.html',
-  styleUrls: ['./foods-list.component.scss']
+  styleUrls: ['./foods-list.component.scss'],
 })
 export class FoodsListComponent implements OnInit {
+  public screen: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    if (window.screen.width > 1024) {
+      this.screen = !this.screen;
+    }
   }
 
+  ngOnInit(): void {}
 }
